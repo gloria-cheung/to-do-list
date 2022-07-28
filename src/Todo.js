31e1729abbef283196f98fb6b6fb53abb324e061
gloria <macuser@MacUsers-MacBook-Pro.local>
2022-07-28 11:30:41 -0400

    add task to state from constrolled form input and use edittodo func from props

--- src/Todo.js
+++ src/Todo.js
@@ -3,23 +3,54 @@ import './Todo.css';
 
 function Todo(props) {
   const [state, setState] = useState({
-    isEditing: false
+    isEditing: false,
+    task: props.task
   })
-  const {task, deleteTodo, id} = props;
+  
+  const {task, deleteTodo, id, editTodo} = props;
+
+  const handleSubmit = function(evt) {
+    evt.preventDefault();
+    const todo = {task: state.task, id};
+    editTodo(todo, id);
+    toggleEdit();
+  };
+
+  const handleChange = function(evt) {
+    setState(st => ({
+      ...st, 
+      task: evt.target.value
+    }))
+    
+  };
+
+  const toggleEdit = function() {
+    setState(st => ({
+      ...st,
+      isEditing: !state.isEditing
+    }));
+  };
 
   let result;
   if (state.isEditing) {
     result = (
       <div className='Todo'>
-        <form>
-          <input type='text' />
+        <form onSubmit={handleSubmit}>
+          <input 
+            type='text'
+            name={state.task}
+            value={state.task}
+            onChange={handleChange}
+          />
+          <button>Save</button>
         </form>
-      </div>
+        <button onClick={toggleEdit}>Cancel</button>
+      </div>  
     )
   } else {
     result = (
       <div className='Todo'>
-        <button onClick={() => {setState({isEditing: true})}}>Edit</button>
+        <button onClick={toggleEdit}>Edit</button>
         <button onClick={() => {deleteTodo(id)}}>X</button>
         <li>
           {task}
