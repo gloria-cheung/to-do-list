c5763bb4537f1026224b0b1725ed4820e473888e
gloria <macuser@MacUsers-MacBook-Pro.local>
2022-07-28 10:48:28 -0400

    add state to see whether editing or not after clicking edit button

--- src/Todo.js
+++ src/Todo.js
@@ -1,17 +1,34 @@
+import {useState} from 'react'
 import './Todo.css';
 
 function Todo(props) {
- const {task, deleteTodo, id} = props;
+  const [state, setState] = useState({
+    isEditing: false
+  })
+  const {task, deleteTodo, id} = props;
 
-  return (
-    <div className="Todo">
-      <button>Edit</button>
-      <button onClick={() => {deleteTodo(id)}}>X</button>
-      <li>
-        {task}
-      </li>
-    </div>
-  );
+  let result;
+  if (state.isEditing) {
+    result = (
+      <div className='Todo'>
+        <form>
+          <input type='text' />
+        </form>
+      </div>
+    )
+  } else {
+    result = (
+      <div className='Todo'>
+        <button onClick={() => {setState({isEditing: true})}}>Edit</button>
+        <button onClick={() => {deleteTodo(id)}}>X</button>
+        <li>
+          {task}
+        </li>
+      </div>
+    )
+  }
+
+  return result;
 }
 
 export default Todo;
