9191819e6b745e077f3ccc0af9e6886dc9178c3d
gloria <macuser@MacUsers-MacBook-Pro.local>
2022-07-28 09:30:22 -0400

    track state for todos and render todo component with props

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,9 +1,24 @@
+import {useState} from 'react'
 import './TodoList.css';
+import Todo from './Todo';
 
 function TodoList() {
+  const [state, setState] = useState({
+    todos: [
+      {task: "walk the dog"},
+      {task: "wash the dishes"},
+      {task: "do laundry"}
+    ]
+  })
+
+  const todos = state.todos.map(todo => <Todo task={todo.task}/>)
+  
   return (
     <div className="TodoList">
-      
+      <h1>Todo List!</h1>
+      <ul>
+        {todos}
+      </ul>
     </div>
   );
 }
